cc579cc0257b522fbcbcf127137b5dbf8ac18ba0
enrique <enrique.melero@gmail.com>
2019-02-20 04:32:40 +0100

    Working copy of the oney creation demo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,133 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Money Creation Simulator</h1>
+    <input v-on:click="askLoan" type=button value="Ask for a Loan">
+    <input v-on:click="buyIco" type=button value="Buy Tokens">
+    <input v-on:click="mineBitcoin" type=button value="Mine Bitcoin">
+    <input v-on:click="buyBitcoin" type=button value="Buy Bitcoin">
+    <h3>Money in the System is {{ totalMoney }}</h3>
+    <div class=bank>
+      <h1>Bank</h1>
+      <div v-for="(amount,account) in this.$store.state.bank">
+        {{ account }} : {{ amount }}<br>
+      </div>
+    </div>
+    <div class=startup>
+      <h1>Startup</h1>
+      <div v-for="amount,account in this.$store.state.startup">
+        {{ account }} : {{ amount }}<br>
+      </div>
+    </div>
+    <div class=me>
+      <h1>Me</h1>
+      <div v-for="amount,account in this.$store.state.me">
+        {{ account }} : {{ amount }}<br>
+      </div>
+    </div>
+    <div class=others>
+      <h1>Others</h1>
+      <div v-for="amount,account in this.$store.state.other">
+        {{ account }} : {{ amount }}<br>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+//import store from "./store";
+
+import store from './store' ;
+// import { mapGetters, mapState } from 'vuex' ;
 
 export default {
+
   name: "app",
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      //myBankLoan: this.$store.state.myBankLoan,
+      bitcoinIsMoney: true,
+      tokenIsMoney: true
+     }
+  },
+  methods: {
+    askLoan: function () {
+      // Accounting in the Banks Ledger
+      this.$store.commit("gl", {
+        gl:"bank",
+        credit:"Client Loan",
+        debit:"Client Deposits",
+        amount:1
+      });
+      // Accounting in the My Ledger
+      this.$store.commit("gl", {
+        gl:"me",
+        credit:"Current Account",
+        debit:"Debt with Bank",
+        amount:1
+      });
+    } ,
+    buyIco: function () {
+      // Accounting in the Others Ledger
+      this.$store.commit("gl", {
+        gl:"other",
+        credit:"Bitcoins",
+        debit:"Tokens",
+        amount:1
+      });
+      // Accounting in the My Ledger
+      this.$store.commit("gl", {
+        gl:"me",
+        credit:"Tokens",
+        debit:"Bitcoins",
+        amount:1
+      });
+    } ,
+    mineBitcoin: function () {
+      // Accounting in the Others Ledger
+      this.$store.commit("gl", {
+        gl:"other",
+        credit:"Bitcoins",
+        debit:"Profit&Loss",
+        amount:1
+      });
+      // Accounting in the My Ledger
+      this.$store.commit("gl", {
+        gl:"me",
+        credit:"Bitcoins",
+        debit:"Current Account",
+        amount:1
+      });} ,
+    buyBitcoin: function () {
+      // Accounting in the Others Ledger
+      this.$store.commit("gl", {
+        gl:"other",
+        credit:"Current Account",
+        debit:"Bitcoins",
+        amount:1
+      });
+      // Accounting in the My Ledger
+      this.$store.commit("gl", {
+        gl:"me",
+        credit:"Bitcoins",
+        debit:"Current Account",
+        amount:1
+      });
+
+    } ,
+  },
+  computed: {
+    totalMoney(){
+      return this.$store.state.myCurrentAcc +
+             this.$store.state.theirCurrentAcc +
+             (this.$data.bitcoinIsMoney? store.state.theirBitcoins + store.state.myBitcoins : 0 ) +
+             (this.$data.tokenIsMoney? store.state.theirTokens + store.state.myTokens : 0 ) ;
+    },
+    myCurrentAcc() { return store.getters.myCurrentAcc },
+    myBankLoan() { return store.getters.myBankLoan },
+
   }
 };
 </script>
@@ -24,5 +140,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.me, .bank, .startup, .others {
+  display: inline-block;
+  *display: inline;
+  vertical-align: top;
+  text-align: left;
+  font-size: 12px;
+  width: 200px;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -4,7 +4,26 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    bank: {"Client Loan":100 , "Client Deposits": -100},
+    me: {"Tokens": 0, "Bitcoins": 0, "Current Account": 10 , "Debt with Bank": 0},
+    other: {"Current Account": 200, "Tokens": 100, "Bitcoins": 100},
+    startup: {"Bitcoins": 100 , "Capital": -100},
+  },
+  mutations: {
+    gl(state, payload) {
+      
+      state[payload.gl][payload.credit]+=payload.amount;
+      state[payload.gl][payload.debit]-=payload.amount;
+    }
+  },
+  actions: {},
+  getters: {
+    myBankLoan (state){
+      return state.myBankLoan;
+    },
+    myCurrentAcc (state){
+      return state.myCurrentAcc;
+    }
+  }
 });
