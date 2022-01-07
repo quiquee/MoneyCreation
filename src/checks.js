71fe42e4ec5767d03d546ab882db5edd7016569b
quiquee <enrique.melero@gmail.com>
2022-01-07 11:35:29 +0100

    Aded balance in journals

---
+++ src/checks.js
@@ -0,0 +1,15 @@
+import {mgmntVariables} from "./variables.js"
+export const checks = {
+    checkprice: function (price){
+        if (price < 0) {
+            return -1 
+        }        
+        return 0
+    },
+    checkpricedao: function(price){
+        if (price > mgmntVariables.maxDaoPrice) {
+            return -1
+        }
+        return 0
+    },
+}
